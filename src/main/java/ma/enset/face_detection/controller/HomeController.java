e21cca09b06b5a230271319895a680a258202c78
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-01-10 02:07:28 +0100

    first step

--- src/main/java/ma/enset/face_detection/controller/HomeController.java
+++ src/main/java/ma/enset/face_detection/controller/HomeController.java
@@ -1,4 +1,24 @@
 package ma.enset.face_detection.controller;
 
-public class HomeController {
+import javafx.fxml.Initializable;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void dashboard() {
+
+    }
+    public void manageUsers() {
+
+    }
+    public void statistics(){
+
+    }
 }
