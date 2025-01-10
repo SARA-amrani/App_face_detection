afb25a156ddec51ae9b8d86a783d7a20a49e4d61
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-01-10 02:20:53 +0100

    first step

---
+++ src/main/java/ma/enset/face_detection/controller/GateGuard.java
@@ -0,0 +1,16 @@
+package ma.enset.face_detection.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GateGuard implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void verify(){
+
+    }
+}
