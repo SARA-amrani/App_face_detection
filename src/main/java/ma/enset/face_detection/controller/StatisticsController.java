875e4518b00239c6a2020458ae4a10237e3e2293
salmaabakil <153354773+salmaabakil@users.noreply.github.com>
2025-01-16 18:02:19 +0100

    Update all (statistics)

--- src/main/java/ma/enset/face_detection/controller/StatisticsController.java
+++ src/main/java/ma/enset/face_detection/controller/StatisticsController.java
@@ -9,7 +9,6 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Label;
 import ma.enset.face_detection.Metier.StatisticsService;
 
 import java.sql.SQLException;
@@ -36,18 +35,16 @@ public class StatisticsController {
         try {
             loadUserStatistics();
             loadAccessAttemptsPerDay();
-            customizePieChartColors();
-            customizeBarChartColors();
+            updateLegendColors();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     private void loadUserStatistics() throws SQLException {
-        int totalUsers = statisticsService.getTotalUsers(); // Nombre total d'utilisateurs
-        int deniedUsers = statisticsService.getDeniedAccessCount(); // Nombre d'accès refusés
+        int totalUsers = statisticsService.getTotalUsers();
+        int deniedUsers = statisticsService.getDeniedAccessCount();
 
-        // Création des données pour le graphique en fonction des résultats réels
         PieChart.Data verifiedData = new PieChart.Data("Verified", totalUsers);
         PieChart.Data deniedData = new PieChart.Data("Denied", deniedUsers);
 
@@ -55,11 +52,8 @@ public class StatisticsController {
         usersPieChart.setData(pieChartData);
     }
 
-
     private void loadAccessAttemptsPerDay() throws SQLException {
         Map<LocalDate, Integer> attemptsPerDay = statisticsService.getAccessAttemptsPerDay();
-
-        ObservableList<XYChart.Series<String, Number>> data = FXCollections.observableArrayList();
 
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName("Access Denied Attempts");
@@ -70,50 +64,30 @@ public class StatisticsController {
             series.getData().add(new XYChart.Data<>(date, attempts));
         }
 
-        data.add(series);
-        attemptsBarChart.setData(data);
-
-        // Appliquer une couleur violette à toute la série
-        applySeriesColor(series, "#4A148C");
-
-        // Forcer la couleur de la légende
-        updateLegendColor("#4A148C");
+        attemptsBarChart.getData().add(series);
     }
 
-    private void applySeriesColor(XYChart.Series<String, Number> series, String color) {
-        for (XYChart.Data<String, Number> data : series.getData()) {
-            data.getNode().setStyle("-fx-bar-fill: " + color + ";");
-        }
-    }
-
-    private void updateLegendColor(String color) {
+    private void updateLegendColors() {
         // Forcer JavaFX à appliquer le CSS et générer les légendes
+        usersPieChart.applyCss();
         attemptsBarChart.applyCss();
 
-        // Modifier les couleurs des éléments de la légende
-        Node legend = attemptsBarChart.lookup(".chart-legend");
-        if (legend != null) {
-            for (Node legendItem : legend.lookupAll(".chart-legend-item-symbol")) {
-                legendItem.setStyle("-fx-background-color: " + color + ";");
+        // Mettre à jour les couleurs des légendes pour le PieChart
+        ObservableList<PieChart.Data> pieData = usersPieChart.getData();
+        String[] pieColors = {"#FF4500", "#FFA500"}; // Couleurs des légendes pour le PieChart
+        for (int i = 0; i < pieData.size(); i++) {
+            String legendSelector = ".chart-legend-item-symbol:nth-of-type(" + (i + 1) + ")";
+            Node legendSymbol = usersPieChart.lookup(legendSelector);
+            if (legendSymbol != null) {
+                legendSymbol.setStyle("-fx-background-color: " + pieColors[i] + "; -fx-background-radius: 0;");
             }
         }
-    }
 
-
-    private void customizePieChartColors() {
-        // Appliquer des couleurs violettes aux secteurs du PieChart
-        for (int i = 0; i < usersPieChart.getData().size(); i++) {
-            String color = (i % 2 == 0) ? "#A68BB3" : "#8C4799"; // Alterner les couleurs
-            usersPieChart.getData().get(i).getNode().setStyle("-fx-pie-color: " + color + ";");
-        }
-    }
-
-    private void customizeBarChartColors() {
-        // Appliquer des couleurs violettes aux barres du BarChart
-        for (XYChart.Series<String, Number> series : attemptsBarChart.getData()) {
-            for (XYChart.Data<String, Number> data : series.getData()) {
-                data.getNode().setStyle("-fx-bar-fill: #8C4799;"); // Couleur violette
-            }
+        // Mettre à jour les couleurs des légendes pour le BarChart
+        String barColor = "#4A148C"; // Couleur violette pour les légendes des barres
+        Node barLegendSymbol = attemptsBarChart.lookup(".chart-legend-item-symbol");
+        if (barLegendSymbol != null) {
+            barLegendSymbol.setStyle("-fx-background-color: " + barColor + "; -fx-background-radius: 0;");
         }
     }
 }
